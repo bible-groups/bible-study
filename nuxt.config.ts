18ca209cc5ed4b002e5733c1fbda359f4005ad04
labri70 <labri70@naver.com>
2025-09-15 16:11:58 +0900

    netlify.toml 파일 삭제 - Netlify 기본 설정 사용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,80 +52,8 @@ export default defineNuxtConfig({
     '~/plugins/auth.client.js'
   ],
   modules: [     
-    '@pinia/nuxt',
-    '@vite-pwa/nuxt'
+    '@pinia/nuxt'
   ],
-  // PWA 설정
-  pwa: {
-    registerType: 'autoUpdate',
-    workbox: {
-      navigateFallback: '/',
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
-    },
-    client: {
-      installPrompt: true,
-      periodicSyncForUpdates: 20
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallbackAllowlist: [/^\/$/],
-      type: 'module'
-    },
-    manifest: {
-      name: 'BIBLE STUDY',
-      short_name: 'BIBLE STUDY',
-      description: '성경의 세계를 더 깊이 탐구하고 신앙 생활을 풍성하게',
-      theme_color: '#3866ef',
-      background_color: '#fff',
-      display: 'standalone',
-      orientation: 'portrait-primary',
-      scope: '/',
-      start_url: '/',
-      icons: [
-        {
-          src: 'images/icons/icon-72x72.png',
-          sizes: '72x72',
-          type: 'image/png'
-        },
-        {
-          src: 'images/icons/icon-96x96.png',
-          sizes: '96x96',
-          type: 'image/png'
-        },
-        {
-          src: 'images/icons/icon-128x128.png',
-          sizes: '128x128',
-          type: 'image/png'
-        },
-        {
-          src: 'images/icons/icon-144x144.png',
-          sizes: '144x144',
-          type: 'image/png'
-        },
-        {
-          src: 'images/icons/icon-152x152.png',
-          sizes: '152x152',
-          type: 'image/png'
-        },
-        {
-          src: 'images/icons/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'images/icons/icon-384x384.png',
-          sizes: '384x384',
-          type: 'image/png'
-        },
-        {
-          src: 'images/icons/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
-  },
   vite: {
     plugins: [
       tailwindcss(),
